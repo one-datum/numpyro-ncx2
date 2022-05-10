4ffb69657f2d13375c2501a7383d79d8b1730733
Dan F-M <foreman.mackey@gmail.com>
2022-05-10 17:04:03 -0400

    fixing tests

--- tests/test_ncx2.py
+++ tests/test_ncx2.py
@@ -2,16 +2,8 @@ import jax
 import numpy as np
 import pytest
 import scipy.stats
-from jax.test_util import _dtype, tolerance
 
 from numpyro_ncx2 import NoncentralChi2
-
-
-def assert_allclose(x, y, *, atol=None, rtol=None):
-    assert x.shape == y.shape
-    atol = 100 * max(tolerance(_dtype(x), atol), tolerance(_dtype(y), atol))
-    rtol = 100 * max(tolerance(_dtype(x), rtol), tolerance(_dtype(y), rtol))
-    np.testing.assert_allclose(x, y, atol=atol, rtol=rtol)
 
 
 @pytest.mark.parametrize("args", [(5, 12.0), (50, 1.1), (1, 80.0)])
@@ -26,4 +18,6 @@ def test_logp(args):
     ref = scipy.stats.ncx2(*args)
     dist = NoncentralChi2(*args)
     samples = dist.sample(jax.random.PRNGKey(4), (5000,))
-    assert_allclose(ref.logpdf(samples), dist.log_prob(samples))
+    np.testing.assert_allclose(
+        ref.logpdf(samples), dist.log_prob(samples), rtol=2e-6
+    )
